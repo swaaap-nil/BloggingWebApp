993fb127a8f29b04bcf322ab3c4cba9c7db86c99
swaaap-nil <swapnilsuman65@gmail.com>
2023-08-06 16:10:37 +0530

    u need to introduce env variables for react

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -21,7 +21,7 @@ import Footer from './components/footer.js';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: 'https://bloggingwebapp-production.up.railway.app/graphql',
   cache: new InMemoryCache(),
 });
 
