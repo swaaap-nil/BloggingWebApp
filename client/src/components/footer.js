bb2f3e9481d2f3553b60b1592faf483fb12886b7
swaaap-nil <swapnilsuman65@gmail.com>
2023-08-08 03:41:25 +0530

    added socials

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { BsGithub,BsTwitter,BsInstagram,BsLinkedin} from "react-icons/bs";
+import {SiLeetcode} from "react-icons/si"
 
 export default function Footer(){
     return <div className="sticky-footer">
@@ -17,35 +19,31 @@ export default function Footer(){
                     <p class="mt-2 text-sm text-gray-500">Design, Code and Ship!</p>
                     <div class="mt-4">
                         <span class="inline-flex justify-center mt-2 sm:ml-auto sm:mt-0 sm:justify-start">
-                            <a class="text-gray-500 cursor-pointer hover:text-gray-700">
-                                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2"
-                                    class="w-5 h-5" viewBox="0 0 24 24">
-                                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                                </svg>
+
+                            {/* github link */}
+                            <a class="text-gray-500 cursor-pointer hover:text-gray-700" href="https://github.com/swaaap-nil">
+                                <BsGithub/>
                             </a>
-                            <a class="ml-3 text-gray-500 cursor-pointer hover:text-gray-700">
-                                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2"
-                                    class="w-5 h-5" viewBox="0 0 24 24">
-                                    <path
-                                        d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z">
-                                    </path>
-                                </svg>
+
+                            {/* leetcode link */}
+                            <a class="ml-3 text-gray-500 cursor-pointer hover:text-gray-700" href="https://leetcode.com/swaaap-nil/">
+                                <SiLeetcode/>
                             </a>
-                            <a class="ml-3 text-gray-500 cursor-pointer hover:text-gray-700">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                    strokeWidth="2" class="w-5 h-5" viewBox="0 0 24 24">
-                                    <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                                    <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                                </svg>
+
+                            {/* twitter link */}
+                            <a class="ml-3 text-gray-500 cursor-pointer hover:text-gray-700" href="https://twitter.com/swaap_nil">
+                                <BsTwitter/>
                             </a>
-                            <a class="ml-3 text-gray-500 cursor-pointer hover:text-gray-700">
-                                <svg fill="currentColor" stroke="currentColor" stroke-linecap="round"
-                                    stroke-linejoin="round" strokeWidth="0" class="w-5 h-5" viewBox="0 0 24 24">
-                                    <path stroke="none"
-                                        d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z">
-                                    </path>
-                                    <circle cx="4" cy="4" r="2" stroke="none"></circle>
-                                </svg>
+
+                            {/* insta link */}
+                            <a class="ml-3 text-gray-500 cursor-pointer hover:text-gray-700" href="https://www.instagram.com/swaap.nil/">
+                                <BsInstagram/>
+                            </a>
+
+                            {/* linkedin link */}
+                            <a class="ml-3 text-gray-500 cursor-pointer hover:text-gray-700" href="https://www.linkedin.com/in/swapnil-suman-7b169b212/">
+                                
+                                <BsLinkedin/>
                             </a>
                         </span>
                     </div>
